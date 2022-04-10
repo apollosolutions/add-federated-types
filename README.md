0c2eaff740a7b390eabbf602d31922dc256f23da
Shane Myrick <shane@apollographql.com>
2022-04-09 17:11:29 -0700

    Initial commit of code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # add-federated-types
 A simple CLI which can add Apollo Federation types to a schema and merge extensions
+
+**The code in this repository is experimental and has been provided for reference purposes only. Community feedback is welcome but this project may not be supported in the same way that repositories in the official [Apollo GraphQL GitHub organization](https://github.com/apollographql) are. If you need help you can file an issue on this repository, [contact Apollo](https://www.apollographql.com/contact-sales) to talk to an expert, or create a ticket directly in Apollo Studio.**
+_______________
+
---
+++ src/default.js
@@ -0,0 +1,29 @@
+import { Command, Option } from 'clipanion';
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+import { printSchemaWithDirectives } from '@graphql-tools/utils';
+import { buildASTSchema } from 'graphql';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+export class DefaultCommand extends Command {
+    schema = Option.String("--schema", { required: true });
+
+    extensions = Option.String("--extensions", { required: true });
+
+    output = Option.String("--out", { required: false });
+
+    async execute() {
+        const schemaWithoutFederation = readFileSync(this.schema, 'utf-8');
+        const federationDefs = readFileSync(path.resolve(path.dirname(''), 'src', 'federation-spec.graphql'), 'utf-8');
+        const extensionsDefs = readFileSync(this.extensions, 'utf-8');
+
+        const merged = buildASTSchema(mergeTypeDefs([schemaWithoutFederation, federationDefs, extensionsDefs]));
+        const sdl = printSchemaWithDirectives(merged);
+
+        if (this.output) {
+            writeFileSync(this.output, sdl);
+        } else {
+            this.context.stdout.write(sdl)
+        }
+    }
+}
